f23eea5250f2888e91994cdbb0d9c87676f70fb9
Alex Nguyen <alexnguyen@Alexs-MacBook-Pro.local>
2016-05-09 16:46:47 -0400

    Added drawing capabilties

--- DrawingApp/ViewController.swift
+++ DrawingApp/ViewController.swift
@@ -12,8 +12,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var lastPoint = CGPoint.zero
+    
     @IBOutlet weak var buttonsStackView: UIStackView!
+    @IBOutlet weak var drawingView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,17 +25,44 @@ class ViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         self.navigationController?.navigationBarHidden = true //Removes title bar
     }
-
+    
     func appBecameActive(){
         //Fix the notifcation center bug
         buttonsStackView.hidden = false
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let point = touch.locationInView(drawingView)
+            self.lastPoint = point
+        }
+        
         buttonsStackView.hidden = true
         //When someone starts a touch
     }
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            //Get the point
+            let point = touch.locationInView(drawingView)
+            //Set the context size
+            UIGraphicsBeginImageContext(self.drawingView.frame.size)
+            //Grab the context
+            let context = UIGraphicsGetCurrentContext()
+            //Set the drawing size
+            self.drawingView.image?.drawInRect(CGRect(x: 0, y: 0, width: self.drawingView.frame.size.width, height: self.drawingView.frame.size.height))
+            //Move content to the last point
+            CGContextMoveToPoint(context, self.lastPoint.x, self.lastPoint.y)
+            //Draw line to the next point
+            CGContextAddLineToPoint(context, point.x, point.y)
+            //Stroke the path
+            CGContextStrokePath(context)
+            //Draw context onto image
+            self.drawingView.image = UIGraphicsGetImageFromCurrentImageContext()
+            //End context editing
+            UIGraphicsEndImageContext()
+            //Reset the last point to the new point.
+            lastPoint = point
+        }
         //When someone moves a touch
     }
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
