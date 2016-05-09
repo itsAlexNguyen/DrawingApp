53bf6703d89347aa4294c372802cbb417b75c0bc
alexnguyen <alexnguyen@alexs-macbook-pro.local>
2016-05-09 14:51:07 -0400

    Initial Commit

---
+++ DrawingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DrawingApp
+//
+//  Created by Alex Nguyen on 2016-05-09.
+//  Copyright © 2016 Alex Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
