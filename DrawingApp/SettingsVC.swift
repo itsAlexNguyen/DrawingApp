4a71f3f38e7791b6ddf6cd7cb5a3f465d5a2c3b0
Alex Nguyen <alexnguyen@Alexs-MacBook-Pro.local>
2016-05-09 16:00:54 -0400

    Finished UI

---
+++ DrawingApp/SettingsVC.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsVC.swift
+//  DrawingApp
+//
+//  Created by Alex Nguyen on 2016-05-09.
+//  Copyright © 2016 Alex Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBarHidden = false
+    }
+
+}
